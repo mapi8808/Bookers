2e105194ce10585767dc654cecb6c1d755ff08a1
mapi8808 <mapi.008.k@gmail.com>
2020-11-16 06:47:36 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,35 @@
 class BooksController < ApplicationController
   def top
+    
   end
   
-  def index
+  def books
+     # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+     @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
- 
- 
   
   def create
-    @books = books.new
+    book = Book.new(books_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
   end
   
+  private
+  def books_params
+    params.permit(:title, :body)
+  end
+  
+  def destroy
+  book = book.find(params[:id])
+  book.destroy
+  redirect_to books_path
+  end
 end
