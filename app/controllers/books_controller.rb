71d40660a2d9abd8a8f3ac66ff692a661c797d4c
mapi8808 <mapi.008.k@gmail.com>
2020-11-19 05:41:38 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,9 +11,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  book = Book.new(books_params)
-  book.save
-  redirect_to book_path(book.id)
+  @book = Book.new(books_params)
+  if @book.save
+  flash[:notice] = "Book was successfully created."
+  redirect_to book_path(@book.id)
+  else
+      render("books")
+  end  
   end
 
   def edit
@@ -21,20 +25,25 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(books_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(books_params)
+     flash[:update] = "Book was successfully updated."
+    redirect_to book_path(@book)
+    else
+       render ("book")
+    end
+  end 
+  
+  def destroy
+   book = Book.find(params[:id])
+   if book.destroy
+   flash[:destroy] = "Book was successfully destroyed."
+   redirect_to books_path
+   end
   end 
   
   private
   def books_params
-   params.permit(:title, :body)
-  end
-  
-  def destroy
-   book = Book.find(params[:id])
-    book.destroy
-   flash[:notice] = "Book was successfully destroyed."
-   redirect_to books_path
+   params.require(:book).permit(:title, :body)
   end
 end
